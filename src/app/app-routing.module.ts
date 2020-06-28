832e264c6d4e7cad16c22c6f25a9e46e6e233dfd
Frederick Mafolo <fmafolo@deloitte.co.za>
2020-06-29 01:12:04 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,52 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { AppComponent } from "./app.component";
+import { BookingFormComponent } from "./booking-form/booking-form.component";
+import { HomeComponent } from "./home/home.component";
 
+// For MDB Angular Pro
+//import { CarouselModule, WavesModule } from 'ng-uikit-pro-standard'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "home", component: HomeComponent },
+  { path: "", redirectTo: "/home", pathMatch: "full" },
+  // { path: 'detail/:id', component: HeroDetailComponent },
+
+  {
+    path: "home",
+    component: HomeComponent,
+    children: [
+      {
+        path: "homebooking",
+        component: BookingFormComponent,
+      },
+      {
+        path: "flights",
+        component: BookingFormComponent,
+      },
+      {
+        path: "hotels",
+        component: BookingFormComponent,
+      },
+      {
+        path: "holidays",
+        component: BookingFormComponent,
+      },
+      {
+        path: "blog",
+        component: BookingFormComponent,
+      },
+      {
+        path: "homecontacts",
+        component: BookingFormComponent,
+      },
+    ],
+  },
+  // { path: '**', component: PageNotFoundComponent },  // Wildcard route for a 404 page
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
